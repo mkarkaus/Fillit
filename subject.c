5166ca88c5cb48eab6581250b3ae673ddea0b5e6
Satu Reijola <sreijola@c3r1p1.hive.fi>
2019-12-04 14:18:16 +0200

    error checks

--- subject.c
+++ subject.c
@@ -1,50 +1,101 @@
-int		ft_checkpc(s)
+int		ft_checkpc(char *s)
 {
-	//check that all chars are allowed
-	//check that every 5th is nline
+	int	i;
+	int	h;
+
+	i = 0;
+	h = 0;
+	while (s[i] != '#')
+		i++;
+	while (s[i])
+	{
+		if (s[i] == '#')
+		{
+			if (i != 0 && s[i - 1] == '#')
+				h++;
+			if (i < 18 && s[i + 1] == '#')
+				h++;
+			if (i > 4 && s[i - 5] == '#')
+				h++;
+			if (i < 15 && s[i + 5] == '#')
+				h++;
+		}
+		i++;
+	}
+	if (h != 6 && h != 8)
+		return (-1);
+	return (0);
+}
+int		ft_checkgrid(char *s)
+{
+	int i;
+	int	h;
+	int	d;
+
+	i = 0;
+	h = 0;
+	d = 0;
+	while (s[i] != '\0' && (s[i] == '.' || s[i] == '#' || s[i] == '\n'))	//check that all chars are allowed
+	{
+		if (s[i] == '.')
+			d++;
+		if (s[i] == '#')
+			h++;
+		i++;
+	}
+	if (h != 4 || d != 12 || s[4] != '\n' || s[9] != '\n' || s[14] != '\n' || 
+			s[19] != '\n' || s[20] != '\n' || s[21] != '\n')
+		return (-1);
+	if (ft_checkpc(s) == -1)
+		return (-1);
+	return (0);
 }
 
 int		main(int ac, char **av)
 {
-	static char	*a;
+	static char	*map;
 	ssize_t ret;
 	char *temp;
 	char buff[BUFF_SIZE + 1];
 
 	ret = 0;
-	if (ac == 2)
+	if (ac != 2)
 	{
-		fd = open(av[1], O_RDONLY); // next loop will read a piece's worth of chars
-		if (a[fd] == NULL && !(a[fd] = ft_strnew(0)))
-			return (-1);
-		while ((ret = read(fd, buff, BUFF_SIZE)))
-		{
-			buff[ret] = '\0';
-			a = ft_strdup(buff); //or big enough strnew right away?
-		}
-		ft_checkpc(a)
+		write (1, "usage: fillit input_file",)
 		return (0);
-		
 	}
-	//check its a valid piece
+	fd = open(av[1], O_RDONLY); // next loop will read a piece's worth of chars
+	if (map == NULL && !(map = ft_strnew(0)))
+		return (-1);
+	while ((ret = read(fd, buff, BUFF_SIZE)))
+	{
+		buff[ret] = '\0';
+		if (ft_checkgrid(buff) == -1)
+		{
+			write(1, "error\n", 6);
+			return (0);
+		}
+		map = ft_strjoin(map. buff); //or big enough strnew right away?
+	}
+	return (0);
 }
 
 
-every Tetrimino must exactly fit in a
-4 by 4 chars square and all Tetrimino are separated by an newline each.
-If the number of parameters sent to your executable is not 1, your program must display
-its usage and exit properly (give info on what is missing)
+// every Tetrimino must exactly fit in a
+// 4 by 4 chars square and all Tetrimino are separated by an newline each.
+// If the number of parameters sent to your executable is not 1, your program must display
+// its usage and exit properly (give info on what is missing)
 
-the smallest is the one at tmost top left corner of the multiple solutions
-trim 12 empties when assembling board
-assign capital letters to # on each piece
+// the smallest is the one at tmost top left corner of the multiple solutions
+// trim 12 empties when assembling board
+// assign capital letters to # on each piece
 
-If the file contains at least one error, your program must display error on the standard output followed by a newline and have to exit properly.
+// If the file contains at least one error, your program must display error on the standard output followed by a newline and have to exit properly.
 
 
-All your sources and headers must be in the same folder. You can have
-two different folders, one for the libft and one for fillit.
+// All your sources and headers must be in the same folder. You can have
+// two different folders, one for the libft and one for fillit.
 
-- pitääkö aluksi tehdä neliölauta jolle mahtuu kaikki palat trimmaamatta?
-- IF \n followed by \n, palikka vaihtuu, jos myös ret == 0 ni tiedoston loppu
+// - pitääkö aluksi tehdä neliölauta jolle mahtuu kaikki palat trimmaamatta?
+// - IF \n followed by \n, palikka vaihtuu, jos myös ret == 0 ni tiedoston loppu
 
