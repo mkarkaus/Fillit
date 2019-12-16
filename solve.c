19cb4f48274612606dfde255fd3f1ffc9f60159b
Miikka Karkaus <mkarkaus@c3r1p1.hive.fi>
2019-12-16 18:55:21 +0200

    tidying it up

--- solve.c
+++ solve.c
@@ -53,35 +53,42 @@ int		ft_solveboard(char ****board, struct piece p, int row, int col)
 	// printf("%s ", p.pcs[p.i][0]);
 	if (!(p.pcs[p.i]))
 		return (0);
-	// if ()
-	else if ((**board)[row] != NULL && ft_fitpc(board, p.pcs[p.i], row, col))
+	if ((**board)[row] != NULL)
 	{
-		ft_putpc(&board, p.pcs[p.i], row, col);
-		printf("%d %d pala\n", row, col);
-		row = 0;
-		while ((**board)[row] != NULL)
+		if (ft_fitpc(board, p.pcs[p.i], row, col))
 		{
-			printf("%s\n", (**board)[row]);
-			row++;
+			ft_putpc(&board, p.pcs[p.i], row, col);
+			printf("%d %d pala\n", row, col);
+			row = 0;
+			while ((**board)[row] != NULL)
+			{
+				printf("%s\n", (**board)[row]);
+				row++;
+			}
+			p.i++;
+			if (ft_solveboard(board, p, 0, 0))
+				return (1);
 		}
-		p.i++;
-		if (ft_solveboard(board, p, 0, 0))
-			return (1);
-	}
-	else if ((**board)[row] != NULL && (**board)[row][col + 1] != '\0')
-	{
+		else if ((**board)[row][col + 1] != '\0')
+		{
 		printf("%d %d merkki etiappai\n", row, col);
-		if (ft_solveboard(board, p, row, col + 1))
-			return (1);
-	}
-	else if ((**board)[row] != NULL && (**board)[row][col + 1] == '\0')
-	{
-		printf("%d %d rivi etiappai\n", row, col);
-		if (ft_solveboard(board, p, row + 1, 0))
-			return (1);
+			if (ft_solveboard(board, p, row, col + 1))
+				return (1);
+		}
+		else if ((**board)[row][col + 1] == '\0')
+		{
+			printf("%d %d rivi etiappai\n", row, col);
+			if (ft_solveboard(board, p, row + 1, 0))
+				return (1);
+		}
 	}
 	else if ((**board)[row] == NULL && p.pcs[p.i][0] != NULL)
-	{
+	// {
+	// 	col++;
+	// 	ft_solveboard(board, p, 0, col);
+	// 		return (1);
+	// }
+	{	
 		if (((**board) = ft_resizeboard(&board)) != NULL)
 		{
 			p.i = 0;
